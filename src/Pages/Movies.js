b2fbde816b762819af489ce16aaabbfdfef91f05
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2023-05-23 16:27:12 +0900

    movie app API 데이터 받아서 화면에 보여주기, Movie, Movies 컴포넌트 분리

---
+++ src/Pages/Movies.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import Movie from '../component/Movie';
+import styles from './Movies.module.css'
+
+const Movies = () => {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(()=>{
+    getMovies();
+  },[]);
+//movie API 데이터를 가져오는 메소드
+  const getMovies = async ()=>{
+    const response = await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year');
+    const jsonMovies = await response.json();
+    setMovies(jsonMovies.data.movies);
+    setLoading(false);
+  }
+  return (
+    <div >
+        {loading?
+           <h1 className={styles.loading}>Loading . . . 🏃🏻‍♀️</h1>
+        :(
+          <>
+            <h1 className={styles.headTitle}>Movies!</h1>
+            <ul className={styles.movies}>
+              {movies.map((ele)=>{
+                return <Movie key={ele.id} movie={ele}/>
+              })}
+            </ul>
+            <footer>
+              <h2 className={styles.footerTItle}> ✻ made by HYERIM KIM</h2>
+              <div>📞 010-2648-2610</div>
+              <div>✉️ helim01033@naver.com</div>
+            </footer>
+          </>
+        )}
+    </div>
+  );
+};
+
+export default Movies;
---
+++ src/component/Movie.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './movie.module.css'
+
+//무비목록 item 컴포넌트
+const Movie = ({movie}) => {
+    const genres = movie.genres;
+    return (
+        <li className={styles.movieItem}>
+            <h3 className={styles.movieItemTitle}>{movie.title}</h3>
+            <section className={styles.movieItemInfo}>
+                <img className={styles.movieItemInfoPoster} src={movie.medium_cover_image}/>
+                <section className={styles.movieItemInfoMain}>
+                    <div className={styles.mmovieItemInfoMainGenre}>genres: {genres.map((e,idx,arr)=>{
+                        return (arr.length-1===idx)?
+                        <>{e}</> :<>{`${e}, `}</>
+                    })}</div>
+                    <div className={styles.movieItemInfoMainDescription}>{movie.description_full}</div>
+                </section>
+            </section>
+        </li>
+    );
+};
+
+export default Movie;
