062c85fb3d29cf0682c3ae98f31ce7acf7300f19
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2023-05-25 16:01:31 +0900

    detail 화면에 정보 뿌려주는 기능 추가

--- src/component/MovieProfile.js
+++ src/component/MovieProfile.js
@@ -2,11 +2,34 @@ import React from 'react';
 import styles from '../component/MovieProfile.module.css'
 
 const MovieProfile = ({movie}) => {
+    let genres = movie.genres;
+    let strGenres = '';
+
+    for(let i in genres){
+        strGenres += genres[i];
+        if(i==(genres.length-1)) break;
+        strGenres+=', '
+    }
+
     return (
         <section className={styles.detailSection}>
             <h2 className={styles.title}>{movie.title}</h2>
             <section className={styles.detailWithoutTitle}>
+                <section className={styles.detailImgSection}>
                 <img className={styles.detailImg} src={movie.medium_cover_image}/>
+                </section>
+                <section className={styles.detailInfoSection}>
+                    <section className={styles.detailSmallInfos}>
+                        <section>genre : {strGenres}</section>
+                        <section className={styles.smallPatition}></section>
+                        <section> rating : {movie.rating}</section>
+                        <section className={styles.smallPatition}></section>
+                        <section >runtime : {movie.runtime}</section>
+                    </section>
+                    <section className={styles.detailInfoDescription}>
+                        {movie.description_intro}
+                    </section>
+                </section>
             </section>
         </section>
     );
